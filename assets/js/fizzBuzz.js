36c266bbdb52e67a08a8764e923dba6ce513cff1
blondabambi <ebba@aniansson.se>
2016-11-25 10:27:42 +0100

    add numbers divisible by three to equal fizz

--- assets/js/fizzBuzz.js
+++ assets/js/fizzBuzz.js
@@ -1,3 +1,8 @@
 function fizzBuzzChallenge(number){
-  return number;
+
+  if (number % 3 == 0) {
+    return 'fizz';
+  } else {
+    return number;
+  };
 };
--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -4,4 +4,7 @@ it('should return 1 if number = 1', function() {
   expect(fizzBuzzChallenge(1)).toEqual(1);
 });
 
+it("should return 'fizz' if number =3", function() {
+  expect(fizzBuzzChallenge(3)).toEqual('fizz');
+});
 });
